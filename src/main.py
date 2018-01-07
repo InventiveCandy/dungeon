310d2fe250644fbc0a10833578c8a6edaf3576e8
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-01-07 17:33:57 +0000

    Added basic game class

--- src/main.py
+++ src/main.py
@@ -2,3 +2,27 @@ import pygame
 from pygame.locals import *
 
 from src import constants
+
+
+"""
+main.py
+
+This is where the magic happens
+"""
+
+
+class Main:
+
+    def __init__(self):
+
+        pass
+
+    def run(self):
+
+        pass
+
+
+if __name__ == "__main__":
+
+    game = Main()
+    game.run()
