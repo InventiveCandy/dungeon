4f0734c8f6b71b68c6c6daa347aab14afaea933f
Jamie Buttenshaw <sigton.scratch@gmail.com>
2018-01-07 00:50:26 +0000

    Added more values to constants.py

--- src/constants.py
+++ src/constants.py
@@ -4,6 +4,15 @@ constants.py
 Various values that won't change
 """
 
+
+# Display dimensions
+
 DISPLAY_WIDTH = 960
 DISPLAY_HEIGHT = 720
 DISPLAY_SIZE = (DISPLAY_WIDTH, DISPLAY_HEIGHT)
+
+
+# Colours
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
